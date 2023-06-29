d55a5ab5ec3fc838d9b2ba32096ff145aec3ad38
lucas-escobar <lucas.an.escobar@gmail.com>
2023-06-29 14:50:59 -0400

    reset to defaults

--- public/web-components/stoic_calendar.js
+++ public/web-components/stoic_calendar.js
@@ -30,12 +30,13 @@ class StoicCalendar extends HTMLElement {
       birthDate: this.getAttribute("birthDate"),
       backgroundColor: 0x696969,
       drawColor: 0xf8f8ff,
-      numCircles: 128,
-      circleRadius: 2,
-      groupRadius: 1.2,
-      segments: 6,
+      numCircles: 12,
+      circleRadius: 0.3,
+      groupRadius: 1.5,
+      segments: 32,
       ndim: 2,
-      wireframe: true,
+      wireframe: false,
+      angularVelocity: 0.0001,
     };
 
     this.cameraSettings = {
@@ -67,12 +68,15 @@ class StoicCalendar extends HTMLElement {
     // generates  52 week x 80 year stoicism calendar
     const [year, month, day] = this.settings.birthDate.split("-");
 
+    const lifeExpectency = 80;
+    const weeksPerYear = 52;
+
     const birthDate = new Date(year, month - 1, day);
     const currentDate = new Date();
     const ageInMs = currentDate - birthDate;
     const ageInWeeks = parseInt(ageInMs / (1000 * 60 * 60 * 24 * 7));
 
-    const calendar = new Array(52 * 80);
+    const calendar = new Array(weeksPerYear * lifeExpectency);
     for (let i = 0; i < calendar.length; i++) calendar[i] = false;
     calendar.splice(0, ageInWeeks, ...new Array(ageInWeeks).fill(true));
 
@@ -92,8 +96,8 @@ class StoicCalendar extends HTMLElement {
     const styleText = `
       :host {
        position: fixed;
-       top: -1;
-       left: -1;
+       top: 0;
+       left: 0;
        width: 100vw;
        height: 100vh;
        z-index: -1;
@@ -102,7 +106,7 @@ class StoicCalendar extends HTMLElement {
 
     sheet.replaceSync(styleText);
 
-    this.shadowRoot.adoptedStyleSheets = [sheet];
+    document.adoptedStyleSheets = [sheet];
   }
 
   _setupRenderer() {
@@ -119,16 +123,10 @@ class StoicCalendar extends HTMLElement {
   }
 
   _generateCircles() {
-    const { scene, camera, renderer } = this.environment;
-    const {
-      numCircles,
-      circleRadius,
-      groupRadius,
-      segments,
-      drawColor,
-      wireframe,
-      ndim,
-    } = this.settings;
+    const scene = new THREE.Scene();
+    const { camera, renderer } = this.environment;
+    const { numCircles, circleRadius, segments, drawColor, wireframe, ndim } =
+      this.settings;
 
     const circleGeometry = new THREE.CircleGeometry(circleRadius, segments);
     const circleMaterial = new THREE.MeshBasicMaterial({
@@ -136,20 +134,7 @@ class StoicCalendar extends HTMLElement {
       wireframe: wireframe,
     });
 
-    function updateCircleOffsets(i, ndim, angle, radius) {
-      offsets[i * ndim] = Math.cos(angle) * radius; // x offset
-      offsets[i * ndim + 1] = Math.sin(angle) * radius; // y offset
-    }
-
-    const offsets = new Float32Array(numCircles * ndim);
-    const totalRadius = circleRadius + groupRadius;
-
-    for (let i = 0; i < numCircles; i++) {
-      // update angle on each iteration
-      const angle = (2 * Math.PI * i) / numCircles;
-      updateCircleOffsets(i, ndim, angle, totalRadius);
-    }
-
+    const offsets = this._getCircleOffsets();
     circleGeometry.setAttribute(
       "offset",
       new THREE.InstancedBufferAttribute(offsets, ndim)
@@ -163,42 +148,96 @@ class StoicCalendar extends HTMLElement {
 
     scene.add(instancedMesh);
 
-    const prevTime = performance.now();
+    const startTime = performance.now();
+    let prevTime = performance.now();
+    let frameCount = 0;
+    let fps = 0;
+    let logInterval = 1000; // log every 1 second
 
-    function animate() {
+    const animate = () => {
       requestAnimationFrame(animate);
-
-      const deltaTime = (performance.now() - prevTime) / 20000;
-
-      const scale = new THREE.Vector3(1, 1, 1);
-      const radius = circleRadius + groupRadius;
-
-      for (let i = 0; i < numCircles; i++) {
-        const angle = (2 * Math.PI * i) / numCircles;
-        const position = new THREE.Vector3(
-          Math.cos(angle + deltaTime) * radius,
-          Math.sin(angle + deltaTime) * radius,
-          0
-        );
-
-        const rotation = new THREE.Quaternion().setFromAxisAngle(
-          new THREE.Vector3(0, 0, -1),
-          (deltaTime * Math.PI) / 10
-        );
-
-        const transformMatrix = new THREE.Matrix4().compose(
-          position,
-          rotation,
-          scale
-        );
-
-        instancedMesh.setMatrixAt(i, transformMatrix);
-      }
-
-      instancedMesh.instanceMatrix.needsUpdate = true;
+      this._updateInstancedCircleTransforms(instancedMesh, startTime);
       renderer.render(scene, camera);
-    }
+
+      const currentTime = performance.now();
+      const deltaTime = currentTime - prevTime;
+      frameCount++;
+
+      if (deltaTime >= logInterval) {
+        fps = Math.round((frameCount * 1000) / deltaTime);
+        console.log(`FPS: ${fps}`);
+        frameCount = 0;
+        prevTime = currentTime;
+      }
+    };
+
     animate();
+  }
+
+  _getCircleOffsets() {
+    // returns array of offsets to be used in instance buffer
+    const { numCircles, circleRadius, groupRadius, ndim } = this.settings;
+
+    const offsets = new Float32Array(numCircles * ndim);
+    const offsetRadius = circleRadius + groupRadius;
+
+    for (let i = 0; i < numCircles; i++) {
+      // update angle on each iteration
+      const angle = (2 * Math.PI * i) / numCircles;
+      offsets[i * ndim] = Math.cos(angle) * offsetRadius; // x offset
+      offsets[i * ndim + 1] = Math.sin(angle) * offsetRadius; // y offset
+    }
+
+    return offsets;
+  }
+
+  _updateInstancedCircleTransforms(instancedMesh, prevTime) {
+    const { numCircles, circleRadius, groupRadius, angularVelocity } =
+      this.settings;
+    const deltaTime = performance.now() - prevTime;
+    const offsetRadius = circleRadius + groupRadius;
+
+    // update all circles
+    for (let i = 0; i < numCircles; i++) {
+      // update angle
+      const angle = (2 * Math.PI * i) / numCircles;
+      const newAngle = angle + angularVelocity * deltaTime;
+
+      // use new angle to transform mesh instances
+      const { position, rotation, scale } = this._getTransformMatrixOptions(
+        newAngle,
+        offsetRadius,
+        deltaTime
+      );
+      const transformMatrix = new THREE.Matrix4().compose(
+        position,
+        rotation,
+        scale
+      );
+
+      instancedMesh.setMatrixAt(i, transformMatrix);
+    }
+
+    instancedMesh.instanceMatrix.needsUpdate = true;
+  }
+
+  _getTransformMatrixOptions(angle, radius, deltaTime) {
+    const position = new THREE.Vector3(
+      Math.cos(angle) * radius,
+      Math.sin(angle) * radius,
+      0
+    );
+
+    const rotationAngle = (deltaTime * Math.PI) / 10;
+    const rotationAxis = new THREE.Vector3(0, 0, 1);
+    const rotation = new THREE.Quaternion().setFromAxisAngle(
+      rotationAxis,
+      rotationAngle
+    );
+
+    const scale = new THREE.Vector3(1, 1, 1);
+
+    return { position, rotation, scale };
   }
 }
 
